e29f847a5b174bf5b487be09fbc463a7747a8b17
Bruno De Luca <brunodeluca_96@hotmail.com>
2019-04-11 03:01:50 -0300

    updated server to use db

--- users-ms/app/userServer.js
+++ users-ms/app/userServer.js
@@ -3,13 +3,20 @@ const services = require('../protoc/user_grpc_pb');
 
 const grpc = require('grpc');
 const config = require('../config');
+const db = require('./db.js');
 
 function GetUser(call, callback) {
+
+    const userFromDB = db.getUserById(call.request.getId());
+
+    console.log(userFromDB);
+
     const reply = new messages.GetUserResponse();
     const user = new messages.User();
-    user.setId(1);
-    user.setName('Bruno');
+    user.setId(userFromDB.id);
+    user.setName(userFromDB.name);
     reply.setUser(user);
+
     callback(null, reply);
 }
 
