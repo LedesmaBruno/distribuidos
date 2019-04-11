577c4ffd8ced60b7f73000b989d0329c99e4e957
Bruno De Luca <brunodeluca_96@hotmail.com>
2019-04-11 03:01:32 -0300

    add db service

---
+++ users-ms/app/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const config = require('../config');
+
+const pool = new Pool(config.pg);
+
+async function getUserById(id) {
+    return await pool.connect()
+        .then(client => {
+            console.log('connected');
+            client.query('SELECT * FROM users WHERE id = $1', [id])
+                .then(res => {
+                    client.release()
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+        .catch(console.error);
+}
+
+module.exports = { getUserById }
