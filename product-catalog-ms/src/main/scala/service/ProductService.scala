965a08a328e8f68298c7782fdad5be3b59206414
Bruno Ledesma <bruno.ledesma96@gmail.com>
2019-04-11 12:18:26 -0300

    Product Catalog implemented with ReactiveMongo

--- product-catalog-ms/src/main/scala/service/ProductService.scala
+++ product-catalog-ms/src/main/scala/service/ProductService.scala
@@ -1,12 +1,9 @@
 package service
 
-import java.util.concurrent.TimeUnit
-
-import catalog.product
 import catalog.product._
 import storage.ProductRepository
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 class ProductService extends ProductServiceGrpc.ProductService {
@@ -15,38 +12,34 @@ class ProductService extends ProductServiceGrpc.ProductService {
 
   implicit private val executionContext: ExecutionContext = ExecutionContext.global
 
-  private val timeOut = Duration(10, TimeUnit.SECONDS)
+  override def healthCheck(request: Ping): Future[Pong] = Future.successful(Pong())
 
   override def getProduct(request: GetProductRequest): Future[GetProductReply] = {
-    val optionalProduct = Await.result(productRepository getProductById request.id, timeOut)
-    Future successful GetProductReply(optionalProduct map (p => p))
+    productRepository.findProductById(request.id)
+      .map(oP => GetProductReply(oP.map(p => p: catalog.product.Product)))
   }
-
-  override def healthCheck(request: product.Ping): Future[product.Pong] = Future.successful(Pong())
 
   override def getAllProducts(request: Empty): Future[AllProducts] = {
-    val products = Await.result(productRepository getAllProducts, timeOut)
-    Future.successful(AllProducts(products map (p => p: catalog.product.Product)))
+    productRepository.getAllProducts
+      .map(ps => AllProducts(ps.map(p => p: catalog.product.Product)))
   }
 
-  //TODO preguntar como determinar el caso de que falle.
   override def saveProduct(request: SaveProductRequest): Future[Empty] = {
     if (request.product.isEmpty) throw new IllegalArgumentException
     val product = request.product.get
-    val optionalResult = Await.result(productRepository save product, timeOut)
-    if (optionalResult.isEmpty)
-      throw new RuntimeException
-    else
+    val writeResult = Await.result(productRepository.save(product), 10 seconds)
+    if (writeResult.ok)
       Future.successful(new Empty)
+    else
+      throw new RuntimeException("Error on save operation.")
   }
 
-  //TODO preguntar como determinar el caso de que falle.
   override def deleteProduct(request: DeleteProductRequest): Future[Empty] = {
-    val optionalResult = Await.result(productRepository deleteById request.id, timeOut)
-    if (optionalResult.isEmpty)
-      throw new RuntimeException
-    else
+    val writeResult = Await.result(productRepository.deleteById(request.id), 10 seconds)
+    if (writeResult.ok)
       Future.successful(new Empty)
+    else
+      throw new RuntimeException("Error on delete operation.")
   }
 
   implicit def convertProductToProtoProduct(product: model.Product): catalog.product.Product =
--- product-catalog-ms/src/main/scala/storage/MongoDB.scala
+++ product-catalog-ms/src/main/scala/storage/MongoDB.scala
@@ -1,15 +1,28 @@
 package storage
 
-import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
-import org.mongodb.scala.MongoClient
-import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
-import org.mongodb.scala.bson.codecs.Macros._
-import model._
+import reactivemongo.api.collections.bson.BSONCollection
+import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object MongoDB {
 
-//  private val mongoClient = MongoClient("mongodb://localhost:27017")
-  private val mongoClient = MongoClient()
-  private val codecRegistry = fromRegistries(fromProviders(classOf[Product]), DEFAULT_CODEC_REGISTRY)
-  val DB = mongoClient.getDatabase("product_catalog").withCodecRegistry(codecRegistry)
+  implicit val exContext = ExecutionContext.global
+
+  private val ip = System.getenv("MONGO_IP")
+  private val mongoUri = "mongodb://" + ip + ":27017/product_catalog"
+  private val driver = new MongoDriver()
+
+  // Connect to the database: Must be done only once per application
+  private val DB = for {
+    parsedUri <- Future.fromTry(MongoConnection.parseURI(mongoUri))
+    connection = driver.connection(parsedUri)
+    databaseName <- Future(parsedUri.db.get)
+    database <- connection.database(databaseName)
+  } yield database
+
+  def getDB: Future[DefaultDB] = DB
+
+  def getCollection(collectionName: String): Future[BSONCollection] = getDB.map(_.collection(collectionName))
+
 }
