84c083e2f061f51ec0ba022cd2c89839718e30e3
MatiasCicilia <matias.cicilia@ing.austral.edu.ar>
2019-05-09 10:47:38 -0300

    Register Product Service on etcd

--- product-catalog-ms/src/main/scala/server/ProductServiceServer.scala
+++ product-catalog-ms/src/main/scala/server/ProductServiceServer.scala
@@ -1,6 +1,10 @@
 package server
 
+import java.net.InetAddress
+
 import catalog.product.ProductServiceGrpc
+import com.ibm.etcd.client.utils.PersistentLeaseKey
+import com.ibm.etcd.client.{EtcdClient, KeyUtils}
 import io.grpc.{Server, ServerBuilder}
 import service.ProductService
 
@@ -8,13 +12,27 @@ import scala.concurrent.ExecutionContext
 
 object ProductServiceServer extends App {
 
-  val server: Server = ServerBuilder.forPort(50000)
+  val port = 50000
+  val server: Server = ServerBuilder.forPort(port)
     .addService(ProductServiceGrpc.bindService(new ProductService(), ExecutionContext.global))
     .build()
 
+  val ip = InetAddress.getLocalHost.getHostAddress
+
   server.start()
-  println("Product service is running...")
+  println(s"Product service is running on: $ip:$port...")
 
   server.awaitTermination()
+
+  def register(myIP: String, myPort: String): Unit = {
+    val etcdClient = EtcdClient.forEndpoint("localhost", 2379).withPlainText.build
+
+    val key = KeyUtils.bs("/services/product/" + myIP + ":" + myPort)
+    val value = KeyUtils.bs(myIP + ":" + myPort)
+
+    val req = etcdClient.getLeaseClient.maintain.minTtl(2).start
+    val plk = new PersistentLeaseKey(etcdClient, req, key, value, null)
+    plk.start()
+  }
 
 }
